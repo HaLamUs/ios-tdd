9b5051b41f8c9dd7527e832b999678f229ed9d49
HaLamUs <itzlamz9x@gmail.com>
2022-10-25 19:38:42 +0700

    feat: S01E01 - no code just storyboard

---
+++ Practice/Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by lamha on 25/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
