b6d1be11983845631c89d562cb277a85fa7cc9b5
HaLamUs <itzlamz9x@gmail.com>
2022-10-28 01:04:23 +0700

    feat: S01E07: delegate select cell

--- QuizApp/QuizApp/QuestionViewController.swift
+++ QuizApp/QuizApp/QuestionViewController.swift
@@ -8,18 +8,22 @@
 import Foundation
 import UIKit
 
-class QuestionViewController: UIViewController, UITableViewDataSource {
+class QuestionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var headerLabel: UILabel!
     
     private var question: String = ""
     private var options = [String]()
+    private let reuseIndentifier = "Cell"
     
-    convenience init(question: String, options: [String]) {
+    private var selection: ((String) -> Void)? = nil
+    
+    convenience init(question: String, options: [String], selection: @escaping (String) -> Void) {
         self.init()
         self.question = question
         self.options = options
+        self.selection = selection
     }
     
     // remove this we need convenience keyword
@@ -32,13 +36,25 @@ class QuestionViewController: UIViewController, UITableViewDataSource {
         headerLabel.text = question
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selection?(options[indexPath.row])
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return options.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = dequeueCell(in: tableView)
         cell.textLabel?.text = options[indexPath.row]
         return cell
+    }
+    
+    private func dequeueCell(in tableView: UITableView) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: reuseIndentifier) {
+            return cell
+        } else {
+            return UITableViewCell(style: .default, reuseIdentifier: reuseIndentifier)
+        }
     }
 }
--- QuizApp/QuizAppTests/QuestionViewControllerTest.swift
+++ QuizApp/QuizAppTests/QuestionViewControllerTest.swift
@@ -11,33 +11,72 @@ import XCTest
 
 class QuestionViewControllerTest: XCTestCase {
     func test_viewDidLoad_rendersQuestionHeaderText() {
-        
-        let sut = QuestionViewController(question: "Q1",options: [])
-        _ = sut.view // we dont call viewDidLoad directlt because it is
+        let sut = makeSUT(question: "Q1", options: [])
+//        _ = sut.view // we dont call viewDidLoad directlt because it is
         XCTAssertEqual(sut.headerLabel.text, "Q1")
     }
     
     func test_viewDidLoad_withNoOptions_rendersZeroOptions() {
-        let sut = QuestionViewController(question: "Q1", options: [])
-        _ = sut.view
+        let sut = makeSUT(question: "Q1", options: [])
         
         XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
     }
     
     func test_viewDidLoad_withOneOptions_rendersOneOptions() {
-        let sut = QuestionViewController(question: "Q1", options: ["A1"])
-        _ = sut.view
+        let sut = makeSUT(question: "Q1", options: ["A1"])
         
         XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 1)
     }
     
     func test_viewDidLoad_withOneOptions_rendersOneOptionText() {
-        let sut = QuestionViewController(question: "Q1", options: ["A1"])
-        _ = sut.view
+        let sut = makeSUT(question: "Q1", options: ["A1"])
         
         let indexPath = IndexPath(row: 0, section: 0)
         let cell = sut.tableView.dataSource?.tableView(sut.tableView, cellForRowAt: indexPath)
         
         XCTAssertEqual(cell?.textLabel?.text, "A1")
+    }
+    
+    func test_viewDidLoad_rendersOptions() {
+
+        XCTAssertEqual(makeSUT(options: ["A1"]).tableView.numberOfRows(inSection: 0), 1)
+        XCTAssertEqual(makeSUT(options: []).tableView.numberOfRows(inSection: 0), 0)
+        XCTAssertEqual(makeSUT(options: ["A1", "A2"]).tableView.numberOfRows(inSection: 0), 2)
+    }
+    
+    func test_viewDidLoad_rendersOptionsText() {
+        XCTAssertEqual(makeSUT(options: ["A1", "A2"]).tableView.title(at: 0), "A1")
+        XCTAssertEqual(makeSUT(options: ["A1", "A2"]).tableView.title(at: 1), "A2")
+    }
+    
+    func test_optionSelected_notifiesDelegate() {
+        var receivedAnwer = ""
+        let sut = makeSUT(options: ["A1"]) {
+            receivedAnwer = $0
+        }
+        
+        let index = IndexPath(row: 0, section: 0)
+        sut.tableView.delegate?.tableView?(sut.tableView, didSelectRowAt: index)
+        XCTAssertEqual(receivedAnwer, "A1")
+        
+    }
+    
+    // MARK: Helper
+    func makeSUT(question: String = "", options: [String] = [], selection: @escaping (String) -> Void = { _ in })
+    -> QuestionViewController {
+        let sut = QuestionViewController(question: question, options: options, selection: selection)
+        _ = sut.view
+        return sut
+    }
+    
+}
+
+private extension UITableView {
+    func cell(at row: Int) -> UITableViewCell? {
+        self.dataSource?.tableView(self, cellForRowAt: IndexPath(row: row, section: 0))
+    }
+    
+    func title(at row: Int) -> String? {
+        cell(at: row)?.textLabel?.text
     }
 }
