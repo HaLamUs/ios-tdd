079425b543e6b1b9dfeba93858ccbc2cb00645e0
HaLamUs <itzlamz9x@gmail.com>
2022-11-03 22:47:41 +0700

    feat: S01E15 finish game test

--- QuizEngineX/QuizEngineXTests/GameTest.swift
+++ QuizEngineX/QuizEngineXTests/GameTest.swift
@@ -10,15 +10,35 @@ import XCTest
 import QuizEngineX // we dont need @testable coz we want to test public interface
 
 class GameTest: XCTestCase {
+    let router = RouterSpy()
+    var game: Game<String, String, RouterSpy>!
+    
+    override func setUp() {
+        super.setUp()
+        game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
+    }
+    
+    func test_startGame_answerZeroOutOfTwoCorrectly_scoresZero() {
+        router.answerCallback("Wrong") //given anwser
+        router.answerCallback("Wrong") // given anwser
+        
+        XCTAssertEqual(router.routedResult!.score, 0)
+        
+    }
     
     func test_startGame_answerOneOutOfTwoCorrectly_scores1() {
-        let router = RouterSpy()
-        startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
-        
-        router.answerCallback("A1")
-        router.answerCallback("Wrong")
+        router.answerCallback("A1") //given anwser
+        router.answerCallback("Wrong") // given anwser
         
         XCTAssertEqual(router.routedResult!.score, 1)
+        
+    }
+    
+    func test_startGame_answerTwoOutOfTwoCorrectly_scores2() {
+        router.answerCallback("A1") //given anwser
+        router.answerCallback("A2") // given anwser
+        
+        XCTAssertEqual(router.routedResult!.score, 2)
         
     }
     
