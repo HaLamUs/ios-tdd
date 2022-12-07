94bf47732590b6116f34a862b3a060aea637105c
HaLamUs <itzlamz9x@gmail.com>
2022-12-07 22:35:43 +0700

    refactor: S02E01 Replace Router by QuizDelegate

--- QuizEngineX/QuizEngineX/Router.swift
+++ QuizEngineX/QuizEngineX/Router.swift
@@ -10,10 +10,19 @@ import Foundation
 /*
  It's a contract that change depend on platform
  */
+@available(*, deprecated)
 public protocol Router {
     associatedtype Answer
     associatedtype Question: Hashable // why because question used as a key in dict
     
     func routeTo(question: Question, answerCallback: @escaping (Answer) -> Void)
     func routeTo(result: ResultX<Question, Answer>)
+}
+
+public protocol QuizDelegate {
+    associatedtype Answer
+    associatedtype Question: Hashable
+    
+    func handle(question: Question, answerCallback: @escaping (Answer) -> Void)
+    func handle(result: ResultX<Question, Answer>)
 }
