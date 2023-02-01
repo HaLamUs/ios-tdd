752c38582e49cb82c75501345bf2c46834f32480
HaLamUs <itzlamz9x@gmail.com>
2023-02-02 00:18:11 +0700

    refactor: S02E07 Deprecated the handle(result:) API in favor of the new didCompleteQuiz(withAnswer:)

--- QuizEngineX/QuizEngineX/QuizDelegate.swift
+++ QuizEngineX/QuizEngineX/QuizDelegate.swift
@@ -11,15 +11,19 @@ public protocol QuizDelegate {
     associatedtype Answer
     associatedtype Question: Hashable
     
-//    // asks the answer
-//    func answer(for question: Question, completion: @escaping (Answer) -> Void)
-//    func didCompleteQuiz(withAnswer: [(question: Question, answer: Answer)]) // array of tuple
+    //    // asks the answer
+    //    func answer(for question: Question, completion: @escaping (Answer) -> Void)
     
     func answer(for question: Question, completion: @escaping (Answer) -> Void)
+    func didCompleteQuiz(withAnswer: [(question: Question, answer: Answer)]) // array of tuple
+    @available(*, deprecated, message: "use didCompleteQuiz(withAnswers: ) instead")
     func handle(result: ResultX<Question, Answer>)
-    
-    
-    //    func handle(result: ResultX<Question, Answer>) <-- We remove ResultX cause it contains score
-    // which the score is hardcode cause in survey dont have score
-    // telling the anwer
+}
+
+#warning("Delete this at some point!")
+public extension QuizDelegate {
+    // default implementation 
+    func answer(for question: Question, completion: @escaping (Answer) -> Void) {
+        
+    }
 }
