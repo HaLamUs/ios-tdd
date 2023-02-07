5f1971eec85e0c7a33defd7aaf004db9d84222d9
HaLamUs <itzlamz9x@gmail.com>
2023-02-08 00:01:27 +0700

    feat: S02E13 make the BasicScore work with generic equatable type

--- QuizApp/QuizApp/Scoring/BasicScore.swift
+++ QuizApp/QuizApp/Scoring/BasicScore.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 final class BasicScore {
-    static func score(for answers: [String], comparingTo correctAnswers: [String]) -> Int {
+    static func score<T: Equatable>(for answers: [T],
+                                    comparingTo correctAnswers: [T]) -> Int {
         return zip (answers, correctAnswers).reduce(0) {
             score, pair in
             return score + (pair.0 == pair.1 ? 1 : 0)
