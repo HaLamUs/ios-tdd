dda060c87b73fd6fae125491a1a0446668c0fc92
HaLamUs <itzlamz9x@gmail.com>
2023-02-07 21:59:29 +0700

    refactor: S02E10 improve the property name

--- QuizEngineX/QuizEngineX/Flow.swift
+++ QuizEngineX/QuizEngineX/Flow.swift
@@ -14,7 +14,7 @@ class Flow<Delegate: QuizDelegate> {
     
     private let delegate: Delegate
     private let questions: [Question]
-    private var newAnswers: [(Question,  Answer)] = []
+    private var answers: [(Question,  Answer)] = []
     
     init(questions: [Question], delegate: Delegate) {
         self.questions = questions
@@ -30,7 +30,7 @@ class Flow<Delegate: QuizDelegate> {
             let question = questions[index]
             delegate.answer(for: question, completion: answer(for: question, at: index))
         } else {
-            delegate.didCompleteQuiz(withAnswer: newAnswers)
+            delegate.didCompleteQuiz(withAnswer: answers)
         }
     }
     
@@ -41,7 +41,7 @@ class Flow<Delegate: QuizDelegate> {
     private func answer(for question: Question, at index: Int) -> (Answer) -> Void {
         return {
             [weak self] answer in
-            self?.newAnswers.replaceOrInsert((question, answer), at: index)
+            self?.answers.replaceOrInsert((question, answer), at: index)
             self?.delegateQuestionHandling(after: index)
         }
     }
