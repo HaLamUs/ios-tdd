0e8dc237fccb3aa71bc3af6274ad4d78cb25c6f2
HaLamUs <itzlamz9x@gmail.com>
2023-02-20 23:31:13 +0700

    feat: Split role data and side effect

---
+++ misscelaneous/SwiftStruct/SwiftStructTests/SwiftStructTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftStructTests.swift
+//  SwiftStructTests
+//
+//  Created by lamha on 20/02/2023.
+//
+
+import XCTest
+@testable import SwiftStruct
+
+class SwiftStructTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
