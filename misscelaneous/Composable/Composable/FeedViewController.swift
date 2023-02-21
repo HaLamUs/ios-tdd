2913975e73c7ce55d38bfe4d5d90004895bdbad5
HaLamUs <itzlamz9x@gmail.com>
2023-02-21 21:26:23 +0700

    feat: Instead of depend on interface we switch to concrete type BUT 	We loose the extensibility

--- misscelaneous/Composable/Composable/FeedViewController.swift
+++ misscelaneous/Composable/Composable/FeedViewController.swift
@@ -11,19 +11,39 @@ protocol FeedLoader {
     func loadFeed(completion: @escaping ([String]) -> Void)
 }
 
+struct Reachability {
+    static let networkAvaiable: Bool = false
+}
+
 class FeedViewController: UIViewController {
+    var remote: RemoteFeedLoader!
+    var local: LocalFeedLoader!
     
-    var loader: FeedLoader!
-    
-    convenience init(loader: FeedLoader) {
+    convenience init(remote: RemoteFeedLoader, local: LocalFeedLoader) {
         self.init()
-        self.loader = loader
+        self.remote = remote
+        self.local = local
     }
     
     func load() {
-        loader.loadFeed {
-            loadedItems in
-            // show next screen
+        if Reachability.networkAvaiable {
+            remote.loadFeed { loadedItems in
+                
+            }
+        } else {
+            local.loadFeed { loadedItems in }
         }
+    }
+}
+
+class RemoteFeedLoader: FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        
+    }
+}
+
+class LocalFeedLoader: FeedLoader {
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        
     }
 }
