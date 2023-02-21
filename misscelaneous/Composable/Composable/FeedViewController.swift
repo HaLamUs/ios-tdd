ffc07216fbc318963ae3af863745fe2ff9ffd9d6
HaLamUs <itzlamz9x@gmail.com>
2023-02-21 22:01:22 +0700

    feat: apply adapter to compose discrete type

--- misscelaneous/Composable/Composable/FeedViewController.swift
+++ misscelaneous/Composable/Composable/FeedViewController.swift
@@ -11,27 +11,19 @@ protocol FeedLoader {
     func loadFeed(completion: @escaping ([String]) -> Void)
 }
 
-struct Reachability {
-    static let networkAvaiable: Bool = false
-}
 
 class FeedViewController: UIViewController {
-    var remote: RemoteFeedLoader!
-    var local: LocalFeedLoader!
+    var loader: FeedLoader!
     
-    convenience init(remote: RemoteFeedLoader, local: LocalFeedLoader) {
+    convenience init(loader: FeedLoader) {
         self.init()
-        self.remote = remote
-        self.local = local
+        self.loader = loader
     }
     
     func load() {
-        if Reachability.networkAvaiable {
-            remote.loadFeed { loadedItems in
-                
-            }
-        } else {
-            local.loadFeed { loadedItems in }
+        loader.loadFeed {
+            loadedItems in
+            // show next screen
         }
     }
 }
@@ -47,3 +39,28 @@ class LocalFeedLoader: FeedLoader {
         
     }
 }
+
+struct Reachability {
+    static let networkAvaiable: Bool = false
+}
+
+// We compose a NEW type (Adapter) from 2 concrete type Remote and Local
+
+class RemoteWithLocalFallbackFeedLoader: FeedLoader {
+    var remote: RemoteFeedLoader!
+    var local: LocalFeedLoader!
+    
+    init(remote: RemoteFeedLoader, local: LocalFeedLoader) {
+        self.remote = remote
+        self.local = local
+    }
+    
+    func loadFeed(completion: @escaping ([String]) -> Void) {
+        let load = Reachability.networkAvaiable ? remote.loadFeed : local.loadFeed
+        load(completion)
+    }
+}
+
+let vc = FeedViewController(loader: RemoteFeedLoader())
+let vc2 = FeedViewController(loader: LocalFeedLoader())
+let vc3 = FeedViewController(loader: RemoteWithLocalFallbackFeedLoader(remote: RemoteFeedLoader(), local: LocalFeedLoader()))
